4cd0ea734f12e3aa29958a34be8957a0193e2866
yasudaProduct <yasu.private.ct@gmail.com>
2025-01-23 22:21:35 +0900

    logger

--- prisma-app/src/app/page.tsx
+++ prisma-app/src/app/page.tsx
@@ -1,7 +1,9 @@
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
+  // SELECT "public"."Post"."id", "public"."Post"."title", "public"."Post"."content", "public"."Post"."published", "public"."Post"."userId" FROM "public"."Post" WHERE 1=1 OFFSET $1
   const posts = await prisma.post.findMany();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16">
       {posts.map((post) => (
--- prisma-app/src/lib/prisma.ts
+++ prisma-app/src/lib/prisma.ts
@@ -1,5 +1,32 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: // https://www.prisma.io/docs/orm/prisma-client/observability-and-logging/logging
+        [
+            {
+                emit: 'event',
+                level: 'query',
+            },
+            {
+                emit: 'event',
+                level: 'info',
+            },
+            {
+                emit: 'event',
+                level: 'warn',
+            },
+            {
+                emit: 'event',
+                level: 'error',
+            },
+        ],
+});
+
+prisma.$on('query', (e: Prisma.QueryEvent) => {
+    console.log('Query: ' + e.query)
+    console.log('Params: ' + e.params)
+    console.log('Duration: ' + e.duration + 'ms')
+    console.log('Target: ' + e.target)
+})
 
 export default prisma;
