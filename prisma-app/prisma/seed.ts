9f3b8312b4ab2b2c2d94beb0f30bbf63253ab938
yasudaProduct <yasu.private.ct@gmail.com>
2025-01-23 21:58:54 +0900

    page

--- prisma-app/prisma/seed.ts
+++ prisma-app/prisma/seed.ts
@@ -1,44 +1,57 @@
 import prisma from "../src/lib/prisma"
 
 async function main() {
-    const alice = await prisma.user.upsert({
-        where: { email: 'alice@prisma.io' },
-        update: {},
-        create: {
-            email: 'alice@prisma.io',
-            name: 'Alice',
-            posts: {
-                create: {
-                    title: 'Check out Prisma with Next.js',
-                    content: 'https://www.prisma.io/nextjs',
-                    published: true,
-                },
+
+    const users = [{
+        email: 'alice@prisma.io',
+        name: 'Alice',
+        posts: [
+            {
+                title: 'title01',
+                content: 'content01',
+                published: true,
+            }
+        ]
+    },
+    {
+        email: 'bob@prisma.io',
+        name: 'Bob',
+        posts: [
+            {
+                title: 'title01',
+                content: 'content01',
+                published: true,
             },
-        },
-    })
-    const bob = await prisma.user.upsert({
-        where: { email: 'bob@prisma.io' },
-        update: {},
-        create: {
-            email: 'bob@prisma.io',
-            name: 'Bob',
-            posts: {
-                create: [
-                    {
-                        title: 'Follow Prisma on Twitter',
-                        content: 'https://twitter.com/prisma',
-                        published: true,
-                    },
-                    {
-                        title: 'Follow Nexus on Twitter',
-                        content: 'https://twitter.com/nexusgql',
-                        published: true,
-                    },
-                ],
+            {
+                title: 'title02',
+                content: 'content02',
+                published: true,
             },
-        },
+
+        ]
+    }
+    ]
+
+    users.map(async (user) => {
+        await prisma.user.upsert({
+            where: { email: user.email },
+            update: {
+                name: user.name,
+                posts: {
+                    create: user.posts,
+                }
+            },
+            create: {
+                email: user.email,
+                name: user.name,
+                posts: {
+                    create: user.posts,
+                }
+            }
+        })
     })
-    console.log({ alice, bob })
+
+    console.log({ users })
 }
 main()
     .then(async () => {
